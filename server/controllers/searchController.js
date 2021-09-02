bf4e34b2e285f4a1f0031c0ce0342078469557b2
Looper222 <dawid.jm123@gmail.com>
2021-09-02 23:32:18 +0200

    Small improvements for a long-term developing

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -4,13 +4,25 @@ const user_search = async (req, res) => {
     const { phrase } = req.body;
     // const phrase = req.query.phrase;
     const reg = new RegExp(phrase, 'i');
-    try {
-        const user = await User.find({ login: { $regex: reg }}).limit(5).lean();
-        console.log(user);
-        res.status(200).json(user);
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({ error: 'User not found'});
+    
+    if (phrase == "") {
+        try {
+            const user = await User.find({ login: { $regex: reg }}).lean();
+            console.log(user);
+            res.status(200).json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(400).json({ error: 'None of users has been found'})
+        }
+    } else {
+        try {
+            const user = await User.find({ login: { $regex: reg }}).limit(5).lean();
+            console.log(user);
+            res.status(200).json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(400).json({ error: 'User not found'});
+        }
     }
 };
 
